9b160b36ad6d4940b5a8df17e09dadc67bb5d42d
Samraj5413 <samraj6083@gmail.com>
2026-03-01 00:47:02 +0530

    chore(domain): fix mistaken Integer ID -> use UUID instead

--- backend/src/main/java/com/sam/marketplace/vendorhub/repository/UserRepository.java
+++ backend/src/main/java/com/sam/marketplace/vendorhub/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.sam.marketplace.vendorhub.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sam.marketplace.vendorhub.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, UUID> {
   Optional<User> findByEmail(String email);
 }
